b3589e7ccd5d777c2cbd43b1942375441cc4a436
Riwaj Bastola <riwajbastola2001@gmail.com>
2026-09-23 22:23:36 -0400

    pyspark

---
+++ class1.py
@@ -0,0 +1,19 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import SparkSession
+
+from enforce_schema import nested_schema, enforce_schema, select
+
+
+def num_rdd():
+    data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    rdd = spark.sparkContext.parallelize(data)
+    print(rdd.collect())
+
+if __name__ == '__main__':
+    spark: SparkSession = SparkSession.builder.master("local[1]").appName("bootcamp.com").getOrCreate()
+
+    # num_rdd()
+
+    # enforce_schema(spark)
+    # nested_schema(spark)
+    select(spark)
---
+++ enforce_schema.py
@@ -0,0 +1,89 @@
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType
+from pyspark.sql import SparkSession
+
+def enforce_schema(spark):
+    data = [("James", "", "Smith", "36636", "M", 3000),
+            ("Michael", "Rose", "", "40288", "M", 4000),
+            ("Robert", "", "Williams", "42114", "M", 4000),
+            ("Maria", "Anne", "Jones", "39192", "F", 4000),
+            ("Jen", "Mary", "Brown", "", "F", -1)
+            ]
+
+    schema = StructType([ \
+        StructField("firstname", StringType(), True), \
+        StructField("middlename", StringType(), True), \
+        StructField("lastname", StringType(), True), \
+        StructField("id", StringType(), True), \
+        StructField("gender", StringType(), True), \
+        StructField("salary", IntegerType(), True) \
+        ])
+
+    df = spark.createDataFrame(data=data, schema=schema)
+    df.printSchema()
+    df.show()
+
+def nested_schema(spark):
+    structureData = [
+        (("James", "", "Smith"), "36636", "M", 3100),
+        (("Michael", "Rose", ""), "40288", "M", 4300),
+        (("Robert", "", "Williams"), "42114", "M", 1400),
+        (("Maria", "Anne", "Jones"), "39192", "F", 5500),
+        (("Jen", "Mary", "Brown"), "", "F", -1)
+    ]
+    structureSchema = StructType([
+        StructField('name', StructType([
+            StructField('firstname', StringType(), True),
+            StructField('middlename', StringType(), True),
+            StructField('lastname', StringType(), True)
+        ])),
+        StructField('id', StringType(), True),
+        StructField('gender', StringType(), True),
+        StructField('salary', IntegerType(), True)
+    ])
+
+    df2 = spark.createDataFrame(data=structureData, schema=structureSchema)
+    df2.printSchema()
+    df2.show(truncate=False)
+
+
+def select(spark):
+    data = [("James", "Smith", "USA", "CA"),
+            ("Michael", "Rose", "USA", "NY"),
+            ("Robert", "Williams", "USA", "CA"),
+            ("Maria", "Jones", "USA", "FL")
+            ]
+    columns = ["firstname", "lastname", "country", "state"]
+    df = spark.createDataFrame(data=data, schema=columns)
+    df.show(truncate=False)
+    df.select(df.firstname, df.lastname).show()
+    df.select(df.columns[:3]).show(3)
+
+
+def nested_structcolumns():
+    data = [
+        (("James", None, "Smith"), "OH", "M"),
+        (("Anna", "Rose", ""), "NY", "F"),
+        (("Julia", "", "Williams"), "OH", "F"),
+        (("Maria", "Anne", "Jones"), "NY", "M"),
+        (("Jen", "Mary", "Brown"), "NY", "M"),
+        (("Mike", "Mary", "Williams"), "OH", "M")
+    ]
+
+    from pyspark.sql.types import StructType, StructField, StringType
+    schema = StructType([
+        StructField('name', StructType([
+            StructField('firstname', StringType(), True),
+            StructField('middlename', StringType(), True),
+            StructField('lastname', StringType(), True)
+        ])),
+        StructField('state', StringType(), True),
+        StructField('gender', StringType(), True)
+    ])
+
+    df2 = spark.createDataFrame(data=data, schema=schema)
+    df2.printSchema()
+    df2.show(truncate=False)  # shows all columns
+
+
+nested_structcolumns()
+
