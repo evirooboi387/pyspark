0c4fa42399b6d3cc5ca69e9a2df32b3dc0a6c471
Riwaj Bastola <riwajbastola2010@gmail.com>
2026-09-24 23:10:48 -0400

    test

--- enforce_schema.py
+++ enforce_schema.py
@@ -164,17 +164,17 @@ def ordering(spark):
     print("After ordering")
 
 
-# def car(spark):
-#     global columns
-#     data = [
-#         ("Ford Torino", 140, 3449, "US"), \
-#         ("Chevrolet Monte Carlo", 150, 3761, "US"), \
-#         ("BMW 2002", 113, 2234, "Europe") \
-#         ]
-#     columns = ["car", "horsepower", "weight", "origin"]
-#     df = spark.createDataFrame(data=data, schema=columns)
-#     df.printSchema()
-#     # df.show(truncate=False)
-#     df.agg(avg("weight")).show()
+def car(spark):
+    global columns
+    data = [
+        ("Ford Torino", 140, 3449, "US"), \
+        ("Chevrolet Monte Carlo", 150, 3761, "US"), \
+        ("BMW 2002", 113, 2234, "Europe") \
+        ]
+    columns = ["car", "horsepower", "weight", "origin"]
+    df = spark.createDataFrame(data=data, schema=columns)
+    df.printSchema()
+    # df.show(truncate=False)
+    df.agg(avg("weight")).show()
 
 
